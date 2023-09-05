69e332f2954cad9714462561773d230474459d97
Kimba SABI N'GOYE <60729256+sabingoyek@users.noreply.github.com>
2023-09-05 20:36:48 +0100

    Update mybackup.sh

--- hands-on-labs/rdba_project/mybackup.sh
+++ hands-on-labs/rdba_project/mybackup.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-mysqldump --all-databases --user=root --password=ODk4LWtpbWJhc2Fi > all-databases-backup.sql
+mysqldump --all-databases --user=root --password=***** > all-databases-backup.sql
 
 current_date=$(date +%Y%m%d)
 backupdir=/tmp/mysqldumps/$current_date
